5e101b329d1af41e0eda8c6408ba57ca63208471
Eliot McIntire <eliotmcintire@gmail.com>
2021-12-08 07:43:29 -0800

    spacing

--- R/project-template.R
+++ R/project-template.R
@@ -138,7 +138,7 @@ setMethod(
     # There is a bug in extractPkgName if the remote account is not present
     modulesSimple <- gsub("(@.+)*(\\(.+)*", "", Require::extractPkgName(modules))
     mods <- paste0("moduleGitRepos <- c('",
-                   paste(modules, collapse = "',\n                     '"), "')")
+                   paste(modules, collapse = "',\n                    '"), "')")
 
     params <- paste0(paste("parameters = list(\n  "), paste(modulesSimple, " = list()", collapse = ",\n  "), "\n)")
 
