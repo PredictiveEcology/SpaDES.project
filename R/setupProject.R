fae180a111f0837b52164721ef429d04ceedcdac
Eliot McIntire <eliotmcintire@gmail.com>
2022-10-11 22:52:18 -0700

    setupProject -- all but git clone etc & config

--- R/setupProject.R
+++ R/setupProject.R
@@ -17,7 +17,14 @@
 #'   managing their git status outside of this function.
 #' @param packages A vector of packages that are needed. This will be passed to
 #'   `Require`
+#' @param setLinuxBinaryRepo Logical. Should the binary RStudio Package Manager be used
+#'   on Linux (ignored if Windows)
+#' @param overwrite Logical. Passed to `getModule`
+#' @param verbose An integer specifying how much verbosity should be shown. Passed
+#'   to various internal functions, including `Require`. Default is 1. The higher the number
+#'   the more verbosity. Set to -1 for almost no messaging.
 #'
+#' @export
 #' @details
 #' This function bundles a sequence of functions: `.libPaths()`, `getModule`, `setwd`,
 #' `Require`
@@ -25,22 +32,58 @@
 #' @importFrom Require extractPkgName
 #' @examples
 #' \dontrun{
-#' setupProject("test", list(modulePath = "m"),
-#'              modules = "PredictiveEcology/Biomass_core",
-#'              packages = "fpCompare")
+#' setupProject(name = "SpaDES.project",
+#'              paths = list(modulePath = "m", projectPath = "~/GitHub/SpaDES.project"),
+#'              modules = c("PredictiveEcology/Biomass_borealDataPrep@development")
+#' )
 #' }
-setupProject <- function(name, paths, modules, packages) {
+setupProject <- function(name, paths, modules, packages, setLinuxBinaryRepo = TRUE,
+                         overwrite = FALSE, verbose = 1) {
   nameSimple <- extractPkgName(name)
   curDir <- getwd()
   inProject <- identical(basename(curDir), nameSimple)
+  isAbs <- unlist(lapply(paths, isAbsolutePath))
+  if (is.null(paths$projectPath)) stop("Please specify paths$projectPath as an absolute path")
+  if (is.null(paths$packagePath))
+    paths$packagePath <- file.path("packages", version$platform, substr(getRversion(), 1, 3))
+  if (is.null(paths$modulePath)) paths$modulePath <- "m"
+  paths[!isAbs] <- lapply(paths[!isAbs], function(x) file.path(paths$projectPath, x))
   paths <- lapply(paths, normPath)
+  paths <- lapply(paths, checkPath, create = TRUE)
   if (!inProject) {
-    browser()
-    checkPath(paths$modulePath, create = TRUE)
-    setwd(paths$modulePath)
+    setwd(paths$projectPath)
   }
 
-  getModule(modules, modulePath = paths$modulePath)
+  # paths <- lapply(paths, normPath)
+  getModule(modules, modulePath = paths$modulePath, overwrite = overwrite)
+  if (isTRUE(setLinuxBinaryRepo))
+    Require::setLinuxBinaryRepo()
+  modulePackages <- packagesInModules(modulePath = paths$modulePath)
+  modulesSimple <- Require::extractPkgName(modules)
+  modulePackages <- modulePackages[modulesSimple]
+  if (missing(packages))
+    packages <- NULL
+  out <- Require::Require(c(unname(unlist(modulePackages)), packages), require = FALSE,
+                          verbose = verbose)
+
+  gitIgnoreFile <- ".gitignore"
+  if (file.exists(gitIgnoreFile)) {
+    gif <- readLines(gitIgnoreFile, warn = FALSE)
+    lineWithPkgPath <- grep(paste0("^", basename(paths$packagePath),"$"), gif)
+    insertLine <- if (length(lineWithPkgPath)) lineWithPkgPath[1] else length(gif) + 1
+    gif[insertLine] <- file.path(basename(paths$packagePath), "*")
+
+    lineWithModPath <- grep(paste0("^", basename(paths$modulePath),"$"), gif)
+    insertLine <- if (length(lineWithModPath)) lineWithModPath[1] else length(gif) + 1
+    gif[insertLine] <- file.path(basename(paths$modulePath), "*")
+
+    writeLines(con = gitIgnoreFile, gif)
+    Require:::messageVerbose(verboseLevel = 1, verbose = verbose,
+                             ".gitignore file updated with packagePath and modulePath; ",
+                             "this may need to be confirmed manually")
+  }
+
+  return(paths)
 }
 
 
@@ -80,3 +123,4 @@ setupProject <- function(name, paths, modules, packages) {
 # 4. setLinuxBinaryRepo()
 # 5. SpaDES.project::packagesInModules
 # 6. Require::Require(c(unname(unlist(outs)), packages), require = FALSE, standAlone = TRUE)
+
