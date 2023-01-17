393ecdbcdd51e478e8b0bb992edcc1a5ef24a551
Eliot McIntire <eliotmcintire@gmail.com>
2023-01-17 12:38:15 -0800

    copy .rndstr to SpaDES.project; no export

--- R/helpers.R
+++ R/helpers.R
@@ -45,3 +45,10 @@ isAbsolutePath <- function (pathnames)
     return(FALSE)
   (components[1L] == "")
 }
+
+.rndstr <- function (n = 1, len = 8) {
+  unlist(lapply(character(n), function(x) {
+    x <- paste0(sample(c(0:9, letters, LETTERS), size = len,
+                       replace = TRUE), collapse = "")
+  }))
+}
