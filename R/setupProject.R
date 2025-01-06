c18150a6f4335e4c074a5a4f408fb785598c1eb9
Susan Murray <murray.e.susan@gmail.com>
2025-01-06 09:36:19 -0800

    studyAreaName2 update: object existence checked outside of function

--- R/setupProject.R
+++ R/setupProject.R
@@ -580,7 +580,11 @@ setupProject <- function(name, paths, modules, packages,
   }
 
   if (identical(params$.globals$.studyAreaName, DEFAULT)) {
-    params <- studyAreaName2(studyArea, rasterToMatch, paths$projectPath, params)
+    params <- studyAreaName2(
+      projectPath = paths$projectPath, params = params,
+      studyArea     = if (exists("studyArea",     inherits = FALSE)) studyArea,
+      rasterToMatch = if (exists("rasterToMatch", inherits = FALSE)) rasterToMatch
+    )
   }
 
   dotsSUB <- Require::modifyList2(dotsSUB, dotsLater)
@@ -3445,9 +3449,9 @@ argsCanGoAnywhere <- c("params", "studyArea", "times")
 DEFAULT <- "DEFAULT"
 
 
-studyAreaName2 <- function(studyArea, rasterToMatch, projectPath, params, verbose = getOption("Require.verbose")) {
-  sa <- if (exists("studyArea", inherits = FALSE)) reproducible::.robustDigest(studyArea) else NULL
-  rtm <- if (exists("rasterToMatch", inherits = FALSE)) paste0(terra::ncell(rasterToMatch), "pix") else NULL
+studyAreaName2 <- function(projectPath, studyArea = NULL, rasterToMatch = NULL, params = NULL, verbose = getOption("Require.verbose")) {
+  sa  <- if (!is.null(studyArea))     reproducible::.robustDigest(studyArea)
+  rtm <- if (!is.null(rasterToMatch)) paste0(terra::ncell(rasterToMatch), "pix")
 
   san <- paste(basename(projectPath), sa, rtm, sep = "_")
   messageVerbose("Setting .studyAreaName to be ", san, " because it is not supplied by user", verbose = verbose)
